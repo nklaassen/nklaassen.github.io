d2515df94821a8069061a8e9fa898f92d4a35e3a
nic <klaassenacoustic@gmail.com>
2016-12-13 16:30:18 -0800

    testing some javascript

---
+++ js/index.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="js/node_modules/dist/jquery.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(() => {
+		$("h1").css("color: red");
+	});
+</script>
