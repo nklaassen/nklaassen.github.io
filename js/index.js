b2d30e8d5f48fe95b77f26354c49de4368824205
nic <klaassenacoustic@gmail.com>
2016-12-13 17:02:46 -0800

    fix js file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-<script type="text/javascript" src="js/node_modules/dist/jquery.min.js"></script>
-<script type="text/javascript">
-	$(document).ready(function() {
-		$("h1").css({"color": "red"});
-	});
-</script>
+$(function() {
+	$("h1").css({"color": "red"});
+});
