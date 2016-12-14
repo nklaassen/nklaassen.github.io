7c608f70ed1c0f0711f36669623300ec8c75267c
nic <klaassenacoustic@gmail.com>
2016-12-13 16:50:55 -0800

    syntax bug fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 <script type="text/javascript" src="js/node_modules/dist/jquery.min.js"></script>
 <script type="text/javascript">
-	$(document).ready(() => {
-		$("h1").css("color: red");
+	$(document).ready(function() {
+		$("h1").css({"color": "red"});
 	});
 </script>
