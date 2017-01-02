64024184fbb7724d8818689035bebe9a2747d65e
nic <klaassenacoustic@gmail.com>
2017-01-02 12:43:58 -0800

    move scripts to head and just keep top bar fixed

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-var scrolled = false;
-var lastPos = 0;
-var topbarHeight = $("#top-bar").outerHeight();
-var delta = 10;
-$(window).scroll(function() {
-	scrolled = true;
-});
-setInterval(function() {
-	if(scrolled)
-		didScroll();
-}, 100);
-function didScroll() {
-	var pos = $(this).scrollTop();
-	if(pos > lastPos + delta && pos > topbarHeight + delta) {
-		$("#top-bar").addClass("hidden");
-		lastPos = pos;
-	}
-	else if(pos < lastPos - delta) {
-		$("#top-bar").removeClass("hidden");
-		lastPos = pos;
-	}
-	scrolled = false;
-}
-
 $(".dropdown-header").click(function(event) {
 	$(this).closest(".dropdown").find(".dropdown-content").slideToggle();
   $(this).find(".dropdown-selector").toggleClass("selected");
